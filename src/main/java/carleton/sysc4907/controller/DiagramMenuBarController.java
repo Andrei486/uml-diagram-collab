2da54ced9ae592be3e810628dd8b7171da91a57a
Andrei Popescu <apopescu486@gmail.com>
2023-10-27 01:32:56 -0400

    Updated bold/italic/underline icons, implemented exit via file menu

--- src/main/java/carleton/sysc4907/controller/DiagramMenuBarController.java
+++ src/main/java/carleton/sysc4907/controller/DiagramMenuBarController.java
@@ -4,8 +4,12 @@ import carleton.sysc4907.model.Diagram;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
@@ -16,6 +20,14 @@ import java.util.logging.Logger;
 
 public class DiagramMenuBarController {
 
+    @FXML
+    private MenuBar menuBar;
+
     public DiagramMenuBarController() {
+    }
+
+    public void closeApplication(ActionEvent actionEvent) {
+        Stage stage = (Stage) menuBar.getScene().getWindow();
+        stage.close();
     }
 }
