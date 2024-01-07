eaafbb4ee00a6b962f43c03e63c29ed3bf443dd7
Andrei Popescu <apopescu486@gmail.com>
2024-01-06 21:24:35 -0500

    Created new functional test

--- src/test/java/carleton/sysc4907/processing/ElementIdManagerTest.java
+++ src/test/java/carleton/sysc4907/processing/ElementIdManagerTest.java
@@ -53,17 +53,6 @@ public class ElementIdManagerTest {
         lenient().when(mockNode.getUserData()).thenReturn(testNodeId);
     }
 
-//    @Test
-//    public void testGetNewId() {
-//        try (MockedStatic<EditingAreaProvider> utilities = Mockito.mockStatic(EditingAreaProvider.class)) {
-//            utilities.when(EditingAreaProvider::getEditingArea).thenReturn(mockEditingArea);
-//            when(mockEditingArea.getChildrenUnmodifiable()).thenReturn(FXCollections.observableList(new LinkedList<>()));
-//            var id = elementIdManager.getNewId();
-//            var expectedLastBits = id & 0xFF;
-//            assertEquals(expectedLastBits, id & 0xFF);
-//        }
-//    }
-
     @Test
     public void getIdExists() {
         try (MockedStatic<EditingAreaProvider> utilities = Mockito.mockStatic(EditingAreaProvider.class)) {
